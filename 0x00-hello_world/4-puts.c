57015df219d7cdde02227b6dd5d1e26d11970b84
root <adanetestfahun3gmail.com>
2021-06-17 10:22:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ int main(void) {
  * Return: Always 0 (Success)
  */
   
-  puts("\"Programming is like building a multilingual puzzle")
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
