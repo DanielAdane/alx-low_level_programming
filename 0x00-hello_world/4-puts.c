b15a73b2c6ef88299e23943752fb4725729c78b5
root <adanetestfahun3gmail.com>
2021-06-17 10:18:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+  
+  puts("\"Programming is like building a multilingual puzzle")
+  return (0);
+}
