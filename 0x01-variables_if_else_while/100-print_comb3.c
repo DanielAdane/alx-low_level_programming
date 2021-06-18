d0847bd5b7a65efb2377f4547d43edc5e8906123
root <adanetestfahun3gmail.com>
2021-06-18 04:57:15 -0700

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,28 +6,27 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
-
-	for (num1 = '0'; num1 <= '9'; num1++)
-	{
-		for (num2 = '0'; num2 <= '9'; num2++)
-		{
-			if (num1 < num2)
-			{
-			putchar(num1);
-			putchar(num2);
-			if (num1 == '8' && num2 == '9')
-			{
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			}
-		}
-	}
-		putchar('\n');
-	return (0);
+int num1;
+int num2;
+for (num1 = '0'; num1 <= '9'; num1++)
+{
+for (num2 = '0'; num2 <= '9'; num2++)
+{
+if (num1 < num2)
+{
+putchar(num1);
+putchar(num2);
+if (num1 == '8' && num2 == '9')
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
