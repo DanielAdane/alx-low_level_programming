2f460afe6189aec1660d957a2c7943b7c8dd5d71
root <adanetestfahun3gmail.com>
2021-06-18 03:06:00 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (num3 = '0'; num3 <= '9'; num3++)
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 == '7' && num2 == '8' && num3 == '9')
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+		putchar('\n');
+	return (0);
+}
