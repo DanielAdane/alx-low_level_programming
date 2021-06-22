41981df9f3944c71c0854f5ab033eaae2c9b7246
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-22 22:50:19 +0300

    print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign -  a function that print the sign of a number
  * @n: The number
- * Return: 
+ * Return:
  * Return 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
