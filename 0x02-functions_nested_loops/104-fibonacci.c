031cd85b19429eab6e46c3e4e70636e887173029
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-23 00:21:55 +0300

    finds and prints the first 98 Fibonacci numbers,  * starting with 1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - program that finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long int a, b, c, d, m, n, z;
+unsigned long int split1, split2, digit, thousand, cycl, limit;
+m = 1;
+n = 2;
+limit = 96;
+digit = 1000;
+printf("%lu, ", m);
+printf("%lu, ", n);
+for (cycl = 1; cycl <= 90; cycl++)
+{
+z = m + n;
+m = n;
+n = z;
+printf("%lu, ", z);
+}
+a = m / digit, b = m % digit;
+c = n / digit, d = n % digit;
+for (cycl = 91; cycl <= limit; cycl++)
+{
+thousand = (b + d) / digit;
+split1 = (a + c) + thousand;
+split2 = (b + d) - thousand * digit;
+a = c, b = d;
+c = split1, d = split2;
+if (split2 >= 100)
+printf("%lu%lu", split1, split2);
+else
+printf("%lu0%lu", split1, split2);
+if (cycl != limit)
+printf(", ");
+}
+putchar('\n');
+return (0);
+}
