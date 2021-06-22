a4211c398378850c644703bb0d978a760ffef575
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-22 23:33:45 +0300

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * jack_bauer - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - a function that prints every minute of the day
  * Return: Always Success
  */
 void jack_bauer(void)
