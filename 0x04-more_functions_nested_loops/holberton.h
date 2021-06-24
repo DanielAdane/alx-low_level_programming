c7e83d11ce91eae9378c528cca39511990de92af
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-24 20:28:57 +0300

    isuper

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBETON_H
+#define HOLBERTON_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int _putchar(char c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
