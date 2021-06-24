6fe4c4d7d4dbafcba112008bf2d314f4902a8333
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-24 19:16:52 +0300

    day, month and year

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -8,4 +8,16 @@ void positive_or_negative(int i);
 #define LARGEST_NUMBER_H
 #include <stdio.h>
 int largest_number(int a, int b, int c);
-#endif /* HOLBERTON_H */
+#endif /* LARGEST_NUMBER_H */
+
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+#include <stdio.h>
+int convert_day(int month, int day);
+#endif /* CONVERT_DAY_H */
+
+#ifndef PRINT_REMAINING_DAYS_H
+#define PRINT_REMAINING_DAYS_H
+#include <stdio.h>
+void print_remaining_days(int month, int day, int year);
+#endif /* PRINT_REMAINING_DAYS_H */
