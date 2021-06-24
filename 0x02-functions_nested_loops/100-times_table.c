37aadeda87279adfc4117d4c1fa09f80b0249d3c
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-24 17:31:59 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,36 @@
 #include "holberton.h"
 /**
- * print_times_table - function that prints the n times table, starting with 0.
- * @n: is greater than 15 or less than 0 the function should not print anything
- * Return: tables
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-int i, j, m, d;
-if ((n < 15) && (n >= 0))
+int num, mult, prod;
+if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (num = 0; num <= n; num++)
 {
-for (j = 0; j <= n; j++)
+_putchar('0');
+for (mult = 1; mult <= n; mult++)
 {
-d = i * j;
-if (j != 0)
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+if (prod >= 100)
 {
-_putchar(44);
-_putchar(32);
-if (d <= 9)
-{
-_putchar(32);
-_putchar(32);
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
-}
-if (d > 99)
+else if (prod <= 99 && prod >= 10)
 {
-m = d / 10;
-_putchar('0' + m / 10);
-_putchar('0' + m % 10);
+_putchar((prod / 10) + '0');
 }
-else if ((d >= 10) && (d <= 99))
-{
-_putchar(32);
-_putchar('0' + d / 10);
-}
-_putchar('0' + d % 10);
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
