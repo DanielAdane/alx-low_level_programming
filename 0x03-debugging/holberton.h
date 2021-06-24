b844b8cae1d63b1c010a049d37e74864a37460a1
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-24 18:51:52 +0300

    largest number

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,3 +3,9 @@
 #include <stdio.h>
 void positive_or_negative(int i);
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+#include <stdio.h>
+int largest_number(int a, int b, int c);
+#endif /* HOLBERTON_H */
