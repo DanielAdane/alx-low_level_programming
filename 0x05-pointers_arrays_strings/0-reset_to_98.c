5c3fab1b31e49a00ae3dddb6d2620e29d61cc81a
ETHIO CODE <adanetesfahun3@gmail.com>
2021-06-29 16:24:42 +0300

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - reasign value in address memory
+ *@n: parameter integer
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _RESET_TO_98
+#define _RESET_TO_98
+void reset_to_98(int *n);
+#endif
